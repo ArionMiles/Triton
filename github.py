35a76a2b23c02225cc695a8c76f04b767023d8d7
Kanishk Singh <singh.kanishk07@gmail.com>
2017-06-09 19:40:37 +0530

    v1.0. Fixes #1, Fixes #3

--- github.py
+++ github.py
@@ -1,4 +1,4 @@
-'''Import requirements'''
+'''Interacts with the GitHub API'''
 import os
 
 import json
@@ -12,74 +12,61 @@ CONFIG = ConfigParser.RawConfigParser()
 CONFIG.read('creds.ini')
 GITTOKEN = CONFIG.get('GITHUB', 'GitToken')
 HEADERS = {'Authorization' : GITTOKEN}
-
 BASEURL = "https://api.github.com/notifications"
-R = requests.get(url=BASEURL, headers=HEADERS)
-#with open('sample.json') as sample:
-#    DICTIONARY = json.loads(sample.read())
-DICTIONARY = json.loads(R.text)
+
 def notifications():
-    '''Main part'''
-    try:
-        if DICTIONARY: #Simple check to see response isn't empty
-            messageContent = notifications2()
-            return messageContent
-    except IndexError as E:
-        print str(E)
-def notifications2():
     '''Check GitHub for Notifications'''
-    try:
-        if not os.path.isfile("notification_id.txt"):
-            notif_id=[]
+    req = requests.get(url=BASEURL, headers=HEADERS)
+    array = json.loads(req.text)
+    if array:
+        try:
+            if not os.path.isfile("updated_at.txt"):
+                unique_id=[]
 
-        else:
-            with open("notification_id.txt", "r") as f:
-                notif_id = f.read()
-                notif_id = notif_id.split('\n')
-                notif_id = list(filter(None, notif_id))
+            else:
+                with open("updated_at.txt", "r") as record:
+                    unique_id = record.read()
+                    unique_id = unique_id.split('\n')
+                    unique_id = list(filter(None, unique_id))
 
-        for i in DICTIONARY:
-            if str(i['id']) not in notif_id:
-                '''Repo Owner Info'''
-                notif_id = str(i['id'])
-                repoOwner = i['repository']['owner']['login']
-                repoOwnerURL = i['repository']['owner']['html_url']
+            for i in array:
+                if str(i['updated_at']) not in unique_id:
+                    #Repo Owner Info
+                    unique_id = str(i['updated_at'])
+                    repoOwner = i['repository']['owner']['login']
+                    repoOwnerURL = i['repository']['owner']['html_url']
 
-                '''Repo Info'''
-                repoName = i['repository']['name']
-                repoURL = i['repository']['html_url']
-                '''Subject'''
-                subjectTitle = i['subject']['title']
-                subjectURL = i['subject']['latest_comment_url']
-                req = requests.get(url=subjectURL, headers=HEADERS)
-                reqdict = json.loads(req.text)
-                #print (reqdict)
-                #for url in reqdict:
-                subjectURL2 = reqdict['html_url']
-                #print (subjectURL2)
-                subjectType = i['subject']['type']
+                    #Repo Info
+                    repoName = i['repository']['name']
+                    repoURL = i['repository']['html_url']
+                   
+                    #Subject
+                    subjectTitle = i['subject']['title']
+                    subjectURL = i['subject']['latest_comment_url']
+                    html_req = requests.get(url=subjectURL, headers=HEADERS)
+                    reqdict = json.loads(html_req.text)
+                    subjectURL2 = reqdict['html_url']
+                    subjectType = i['subject']['type']
 
-                '''Message'''
-                messageContent = '\nFrom: [{repoOwner}]({repoOwnerURL}) \
+                    #Message Content
+                    messageContent = '\nFrom: [{repoOwner}]({repoOwnerURL}) \
                       \nRepository: [{repoName}]({repoURL})\
                       \nSubject: [{subjectTitle}]({subjectURL})\
                       \nType: {subjectType}' .format(repoOwner = repoOwner, repoOwnerURL=repoOwnerURL,
-                                repoName=repoName, repoURL=repoURL, subjectTitle=subjectTitle, subjectURL=subjectURL2,
-                                subjectType=subjectType)
-                f = open('notification_id.txt', 'a')
-                f.write (notif_id + '\n')
-            else:
-                messageContent = None
-                #return messageContent
+                        repoName=repoName, repoURL=repoURL, subjectTitle=subjectTitle, subjectURL=subjectURL2,
+                        subjectType=subjectType)
+                    record = open('updated_at.txt', 'a')
+                    record.write (unique_id + '\n')
+                else:
+                    messageContent = None
+                    #return messageContent
 
-    except IndexError as E:
-        print 'IndexError. Reason: %s' % str(E)
-    except KeyError as E:
-        print 'KeyError. Reason: %s' % (str(E))
+        except IndexError as E:
+            print 'IndexError. Reason: %s' % str(E)
+        except KeyError as E:
+            print 'KeyError. Reason: %s' % (str(E))
 
-    return messageContent
-#notifications()
+        return messageContent
+
 if __name__ == '__main__':
-    pass
-    #notifications()
-    #this needs fixing
+    notifications()
